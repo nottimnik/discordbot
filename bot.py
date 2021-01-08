dc4d005c78818fc0ff17088bc4a4a7df1a4aa291
Timnik <72356939+Timnikk@users.noreply.github.com>
2021-01-08 12:04:46 -0800

    Added Giveaway Commands

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import random
 import wikipedia
+import asyncio
+import datetime
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
@@ -29,6 +31,7 @@ async def help(ctx):
     em.add_field(name = "😂 Fun", value = "`!help fun`")
     em.add_field(name = "💬 Social", value = "`!help social`")
     em.add_field(name = "📣 Polls", value = "`!help polls`", inline = False)
+    em.add_field(name = "🎉 Giveaway", value = "`!help giveaways`")
     await ctx.send(embed = em)
 
 @help.command()
@@ -62,9 +65,15 @@ async def social(ctx):
 
 @help.command()
 async def polls(ctx):
-    em = discord.Embed(title = "Poll Commands")
+    em = discord.Embed(title = "📣 Polls Commands")
     em.add_field(name = "!poll [message]", value = "`Creates a poll with the introduced message.`")
     await ctx.send(embed = em)
+
+@help.command()
+async def giveaways(ctx):
+    em = discord.Embed(title = "🎉 Giveaways Commands")
+    em.add_field(name = "!giveaway", value = "`Creates a new giveaway.`")
+    em.add_field(name = "!reroll [channel] [id of the giveaway]", value = "`Rerolls the winners of the giveaway.`")
 
 ##Moderation Commands:
 @client.command(aliases=["c"]) ##The Clear Command
@@ -188,7 +197,7 @@ async def hug(ctx, member: discord.Member):
 async def kiss(ctx, member : discord.Member):
     await ctx.send(f"{member.mention} someone gave you a kiss :kissing_heart:")
 
-#Poll Commands:
+#Polls Commands:
 @client.command()
 async def poll(ctx,*,message):
     try:
@@ -198,5 +207,104 @@ async def poll(ctx,*,message):
         await msg.add_reaction("👎")
     except:
         await ctx.send("Invalid Syntax! Please use the correct version: **`!poll [message]`**")
+
+#Giveaway Commands:
+def convert(time):
+    pos = ["s","m","h","d"]
+
+    time_dict = {"s" : 1, "m" : 60, "h" : 3600, "d" :3600*24}
+
+    unit = time[-1]
+
+    if unit not in pos:
+        return -1
+    try:
+        val = int(time[:-1])
+    except:
+        return -2
+
+    return val * time_dict[unit]
+
+@client.command()
+@commands.has_permissions(administrator = True)
+async def giveaway(ctx):
+    await ctx.send("Let's start with this giveaway! Answer these questions within 15 seconds!")
+
+    questions = ["Which channel should it be hosted in?",
+    "What should be the duration of the giveaway? (s|m|h|d)",
+    "What is the prize of the giveaway?"]
+
+    answers = []
+
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+
+    for i in questions:
+        await ctx.send(i)
+
+        try:
+            msg = await client.wait_for("message", timeout=15.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send("You didn\'t answer in time, please be quicker next time!")
+            return
+        else:
+            answers.append(msg.content)
+    
+    try:
+        c_id = int(answers[0][2:-1])
+    except:
+        await ctx.send(f"You didn't mention a channel properly. Do it like this {ctx.channel.mention} next time.")
+        return
+    
+    channel = client.get_channel(c_id)
+
+    time = convert(answers[1])
+    if time == -1:
+        await ctx.send(f"You didn't answer the time with a proper unit. Use (s|m|h|d) next time!")
+        return 
+    elif time == -2:
+        await ctx.send(f"The time must be an integer. Please enter an integer next time!")
+        return
+
+    prize = answers[2]
+     
+    await ctx.send(f"The Giveaway wille be in {channel.mention} and will last {answers[1]} seconds!")
+
+    em = discord.Embed(title = "Giveaway!", description = f"{prize}")
+    em.add_field(name = "Hosted by:", value = ctx.author.mention)
+    em.set_footer(text= f"Ends {answers[1]} from now!")
+
+    my_msg = await channel.send(embed = em)
+
+    await my_msg.add_reaction("🎉")
+
+    await asyncio.sleep(time)
+
+    new_msg = await channel.fetch_message(my_msg.id)
+
+    users = await new_msg.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+
+    winner = random.choice(users)
+
+    await channel.send(f"Congratulations! {winner.mention} won {prize}!")
+
+@client.command()
+@commands.has_permissions(administrator = True)
+async def reroll(ctx, channel : discord.TextChannel, id_ : int):
+    try: 
+        new_msg = await channel.fetch_message(id_)
+    except:
+        await ctx.send("The id was entered incorrectly.")
+        return
+
+    users = await new_msg.reactions[0].users().flatten()
+    users.pop(users.index(client.user))
+
+    winner = random.choice(users)
+
+    await channel.send(f"Congratulations! The new winner is {winner.mention}!")
+
+
 
 client.run("TOKEN")
